d14c7960bde2c9940ea5582bbab3ace6c3ab6f44
Tal <tal.yaron@gmail.com>
2020-10-19 04:00:11 +0300

    add submit on shift+enter

--- src/views/Commons/Chat/Input/Input.js
+++ src/views/Commons/Chat/Input/Input.js
@@ -1,5 +1,5 @@
 import m from 'mithril';
-    import './Input.css';
+import './Input.css';
 
 //components
 
@@ -7,35 +7,45 @@ import m from 'mithril';
 import store from '../../../../data/store';
 
 //functions
-import {sendMessage} from '../../../../functions/firebase/set/set';
+import { sendMessage } from '../../../../functions/firebase/set/set';
 
 
 module.exports = {
-    oninit:vnode=>{
-        vnode.state = {message:''}
+    oninit: vnode => {
+        vnode.state = { message: '' }
     },
-    view:vnode=>{
-        const {groupId, questionId, subQuestionId, optionId} = vnode.attrs.ids;
-        const {title, entity, topic, url} = vnode.attrs;
-        return(
-            <form class='input' onsubmit={e=>handleSend(e,{title, entity, topic, url, groupId, questionId, subQuestionId, optionId, user:store.user, message:vnode.state.message, vnode})}>
-                <button type='submit' class='input__send' >
+    view: vnode => {
+        const { groupId, questionId, subQuestionId, optionId } = vnode.attrs.ids;
+        const { title, entity, topic, url } = vnode.attrs;
+        return (
+            <div class='input'>
+                <button type='submit' class='input__send' onclick={() => handleSend({ title, entity, topic, url, groupId, questionId, subQuestionId, optionId, user: store.user, message: vnode.state.message, vnode })}>
                     <img src='img/send-24px.svg' alt='send' />
                 </button>
-                <textarea onkeyup={e=>{
-                    console.log(e.target.value)
-                    vnode.state.message = e.target.value}}>
+                <textarea
+                    value={vnode.state.message}
+                    onkeyup={e => {
+
+                        vnode.state.message = e.target.value
+
+                        //submit on enter+shift
+                        if (e.key === 'Enter' && e.shiftKey === true) {
+
+                            handleSend({ title, entity, topic, url, groupId, questionId, subQuestionId, optionId, user: store.user, message: vnode.state.message, vnode })
+                        }
+                    }}>
 
                 </textarea>
-                
-            </form>
+
+            </div>
         )
     }
 }
 
-function handleSend(e,options){
-    e.preventDefault();
-    console.log(options)
+function handleSend(options) {
+
+
+    const { vnode } = options;
     sendMessage(options)
-    e.target.reset();
+    vnode.state.message = '';
 }
