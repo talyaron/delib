1e5b984812ec895fb81c685073b5585437000cca
Tal <tal.yaron@gmail.com>
2021-02-02 18:06:33 +0200

    service worker

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,46 @@
+const SITE_STATIC = 'site-static';
+const assets = [
+    '/',
+    '/js/velocity.js',
+    '/settings.js',
+    '/img/new.svg',
+    '/img/hamburger.svg',
+    '/img/add_alert.svg',
+    '/img/home-24px.svg',
+    '/img/feed.svg',
+    '/img/messages.svg',
+    '/img/addToHome.svg',
+    '/manifest.webmanifest',
+    'https://fonts.googleapis.com/css?family=Varela+Round',
+    'https://fonts.gstatic.com/s/varelaround/v13/w8gdH283Tvk__Lua32TysjIfp8uPLdshZg.woff2',
+    'https://fonts.gstatic.com/s/varelaround/v13/w8gdH283Tvk__Lua32TysjIfpcuPLdshZhVB.woff2'
+
+]
+
 self.addEventListener('install', installEvn => {
     console.log('sw has been installed', installEvn)
+    const preCache = async () => {
+        const cache = await caches.open(SITE_STATIC);
+        console.log('cached')
+        return cache.addAll(assets);
+      };
+      installEvn.waitUntil(preCache());
+
 })
 
-self.addEventListener('activate',activationEvt=>{
+self.addEventListener('activate', activationEvt => {
     console.log('.........sw was activated', activationEvt);
 })
 
-self.addEventListener('fetch', ev=>{
-    console.log('fetch event', ev.request.url)
+self.addEventListener('fetch', ev => {
+    console.log('fetch event', ev.request.url);
+
+    ev.respondWith(
+        caches.match(ev.request)
+        .then(cacheRes=>{
+            return cacheRes || fetch(ev.request)
+        })
+    )
 })
 
 // self.addEventListener('fetch', event => {
