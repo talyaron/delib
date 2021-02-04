7694d125ea8655c5aa7a7bd2b9c6838af99db8cb
Tal <tal.yaron@gmail.com>
2021-02-04 08:00:18 +0200

    cont

--- src/views/Commons/NavBottom/NavBottom.js
+++ src/views/Commons/NavBottom/NavBottom.js
@@ -21,34 +21,6 @@ module.exports = {
 
    
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-      console.log('beforeinstallprompt..................................')
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      // Update UI to notify the user they can add to home screen
-      vnode.state.showInstall = true;
-      m.redraw()
-
-      installBtn.addEventListener('click', (e) => {
-        console.log('wait............')
-        // hide our user interface that shows our A2HS button
-        node.state.showInstall = false;
-        m.redraw();
-        // Show the prompt
-        deferredPrompt.prompt('Would you like to make this a real app?');
-        // Wait for the user to respond to the prompt
-        deferredPrompt.userChoice.then((choiceResult) => {
-          if (choiceResult.outcome === 'accepted') {
-            console.log('User accepted the A2HS prompt');
-          } else {
-            console.log('User dismissed the A2HS prompt');
-          }
-          deferredPrompt = null;
-        });
-      });
-    });
 
 
   },
