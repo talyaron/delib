dfe112bbd8b70692f4c5a6ff91125a12a2297984
Tal <tal.yaron@gmail.com>
2021-10-26 18:19:36 +0300

    finised setQuestions

--- src/functions/firebase/set/setQuestions.js
+++ src/functions/firebase/set/setQuestions.js
@@ -1,15 +1,13 @@
 import { DB } from '../config';
+import { doc, addDoc, setDoc } from "firebase/firestore";
 import store from '../../../data/store';
 
 export function createReactions({ groupId, questionId, title }) {
     return new Promise((resolve, reject) => {
-        DB.collection('groups')
-            .doc(groupId)
-            .collection('questions')
-            .doc(questionId)
-            .collection('reactions')
-            .doc('info')
-            .set({ groupId, questionId, title }, { merge: true })
+
+        const infoRef = doc(DB, 'groups', groupId, 'questions', questionId, 'reactions', 'info')
+
+        setDoc(infoRef, { groupId, questionId, title }, { merge: true })
             .then(() => {
                 resolve(true)
             })
@@ -27,14 +25,10 @@ export function setNewReaction({ groupId, questionId, subQuestionId, type }) {
         if (!questionId) throw new Error('no questionId was given')
         if (!type) throw new Error('no type was given')
 
-        DB.collection('groups')
-            .doc(groupId)
-            .collection('questions')
-            .doc(questionId)
-            .collection('subQuestions')
-            .doc(subQuestionId)
-            .collection('reactions')
-            .add({ userId: store.user.uid, reactionType: type, dateSeconds: (new Date().getTime()/1000) })
+        const reactionsRef = doc(DB, 'reactions', groupId, 'questions', questionId, 'subQuestions', subQuestionId, 'reactions')
+
+
+        addDoc(reactionsRef, { userId: store.user.uid, reactionType: type, dateSeconds: (new Date().getTime() / 1000) })
             .then(() => {
                 // console.info('New reaction was set by user', store.user.uid)
             })
